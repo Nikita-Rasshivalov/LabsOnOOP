eaa1f85616e48f3c92b1b2d71bf0358b1b17983d
Nikita <nikita_230201@mail.ru>
2020-11-17 18:16:39 +0300

    Laba4

---
+++ lab4/WorkWithStrings/Strings/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using WorkWithStrings;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace Strings
+{
+    class UI
+    {
+        static void Main(string[] args)
+        {
+            int exit = 3;
+            int choice = 0;
+            while (choice != exit)
+            {
+                choice = GetChoice();
+                Console.Clear();
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Введите исходную  строку");
+                        string initialString = Console.ReadLine();
+
+                        string pattern = @"[a-zA-Z]";
+                        if (Regex.IsMatch(initialString, pattern))
+                        {
+                            Console.WriteLine("Строка должна содержать только русские символы! ");
+                        }
+                        else
+                        {
+                            string newString = Words.DelRepeatedWord(initialString);
+                            Console.WriteLine($"Новая строка:{newString}");
+                        }
+
+                        break;
+                    case 2:
+                        Console.WriteLine("Введите исходную  строку");
+                        Console.WriteLine("Пример ввода: vk.com 24.12.2002 16-59.");
+                        string dataString = Console.ReadLine();
+                        StringBuilder result = Words.GetString(dataString);
+                        
+                        if (result != null)
+                        {
+                            Console.WriteLine(result);
+                            
+                        }
+                        else
+                        {
+                            Console.WriteLine("Данные введены некорректно ");
+                        }
+                        break;
+
+                    default:
+                        Console.WriteLine("Ошибка ввода");
+                        break;
+                }
+            }
+        }
+        static int GetChoice()
+        {
+            int choice;
+            Console.WriteLine("Выберите:\n1.Получить в новой строке отредактированный текст в котором удалены подряд идущие вхождения слов в предложении\n" +
+                "2.Получить строку из адресов с датой подключения(подключение осущетсвялось  1 раз), .\n3.Выход");
+            string input = Console.ReadLine();
+            int.TryParse(input, out choice);
+            return choice;
+        }
+    }
+}
---
+++ lab4/WorkWithStrings/WorkWithStrings/Words.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace WorkWithStrings
+{
+    public static class Words
+    {
+        public static string DelRepeatedWord(string myString)
+        {
+            string symbols = Regex.Replace(myString, "[^!.]", "");
+            int numSymbol = 0;
+            string newString = "";
+            string[] sentences = myString.Split(new char[] { '.', '!' }, StringSplitOptions.RemoveEmptyEntries);
+            bool flag = true;
+            if (symbols.Length != sentences.Length)
+            {
+                newString = "Неверно введена строка";
+                flag = false;
+            }
+            if (flag == true)
+            {
+                foreach (var sentence in sentences)
+                {
+                    string[] words = sentence.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    newString += CheckRepeatWords(words) + symbols[numSymbol];
+                    numSymbol++;
+
+                }
+            }
+            return newString;
+        }
+
+        public static string CheckRepeatWords(string[] words)
+        {
+            string newString = "";
+            for (int i = 0; i < words.Length - 1; i++)
+            {
+                bool flag = true;
+                int j = i + 1;
+                while (flag == true && j < words.Length)
+                {
+                    int compareWords = string.Compare(words[i].ToLower(), words[j].ToLower());
+                    if (compareWords == 0)
+                    {
+                        words[j] = "";
+                    }
+                    else
+                    {
+                        if (words[j] != "")
+                        {
+                            flag = false;
+                        }
+                    }
+                    j++;
+                }
+            }
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i] != "")
+                {
+                    newString += words[i] + " ";
+                }
+
+            }
+            return newString;
+        }
+
+        public static bool CheckAdressAndDate(string[] sentences)
+        {
+            bool flag = true;// Check correсting input
+            string pattern = @"^\w+\.\w+\s+(\d{2}\.\d{2}\.\d{4}|\d{2}/\d{2}/\d{2}|\d{2}\.\d{2}\.\d{2})\s+([0-2][0-3]-[0-5][0-9]|[0-1][0-9]-[0-5][0-9])$";
+            foreach (var sentence in sentences)
+            {
+                if (!Regex.IsMatch(sentence.Trim(), pattern, RegexOptions.IgnoreCase))
+                {
+                    flag = false;
+                }
+            }
+            return flag;
+        }
+
+        public static StringBuilder GetString(string myString)
+        {
+            string[] separator = { ". " };
+            myString += " ";
+            string[] sentences = myString.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+            Console.WriteLine(sentences.Length);
+            bool flag = CheckAdressAndDate(sentences);
+            StringBuilder newString = null;
+
+            if (flag == true)
+            {
+                newString = new StringBuilder();
+
+                foreach (var sentence in sentences)
+                {
+                    string[] empty = { " " };
+                    string[] items = sentence.Split(empty, StringSplitOptions.RemoveEmptyEntries);
+                    int counterNeedSentences = 0;
+                    for (int i = 0; i < sentences.Length; i++)
+                    {
+                        string[] needItem = sentences[i].Split(empty, StringSplitOptions.RemoveEmptyEntries);
+
+                        if (needItem[0] == items[0])
+                        {
+                            counterNeedSentences++;
+                        }
+
+                    }
+                    if (counterNeedSentences == 1)
+                    {
+                        newString.Append(sentence + separator[0]);
+                    }
+
+                }
+
+            }
+            return newString;
+        }
+
+    }
+}
