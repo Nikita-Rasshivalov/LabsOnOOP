b5b4e70fbf6df1725e4be56285b2279498ca8e85
Nikita <nikita_230201@mail.ru>
2020-11-24 18:50:37 +0300

    first commit

---
+++ lab6/FishingShop/ShopFishing/DayInfo.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShopFishing
+{
+    public enum FishingThings
+    {
+        FishingRod,
+        Spinning,
+        Fider,
+        Picker
+    }
+
+    public enum FishingType
+    {
+        Summer,
+        Winter
+    }
+
+
+    public class DayInfo
+    {
+        public class InfoAboutPurchase
+        {
+
+            public FishingType FishingType { get; set; }
+            public DateTime Date { get; set; }
+            public FishingThings ThingType { get; set; }
+
+            public InfoAboutPurchase(FishingThings thingType, DateTime date)
+            {
+                Date = date;
+                ThingType = thingType;
+            }
+
+            public InfoAboutPurchase()
+            {
+
+            }
+        }
+
+        public List<InfoAboutPurchase> Purchases;
+        public DateTime Date;
+        public DayInfo(DateTime date, List<InfoAboutPurchase> purchases)
+        {
+            this.Date = date;
+            this.Purchases = purchases;
+        }
+        public int FishingRodNumber { get { return GetNumber(FishingThings.FishingRod); } }
+        public int Spinning { get { return GetNumber(FishingThings.Spinning); } }
+        public int Fider { get { return GetNumber(FishingThings.Fider); } }
+        public int Picker { get { return GetNumber(FishingThings.Picker); } }
+
+        private int GetNumber(FishingThings type)
+        {
+            int number = 0;
+            foreach (var purchase in Purchases)
+            {
+                if (purchase.ThingType == type)
+                    number++;
+            }
+            return number;
+        }
+
+
+
+
+    }
+}
