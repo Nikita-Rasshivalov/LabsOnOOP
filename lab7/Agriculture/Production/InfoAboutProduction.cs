7f47d95a95a8a18ce3ba82420e98d4af36f1ac95
Nikita <nikita_230201@mail.ru>
2020-12-01 20:55:16 +0300

    recommmit

--- lab7/Agriculture/Production/InfoAboutProduction.cs
+++ lab7/Agriculture/Production/InfoAboutProduction.cs
@@ -4,46 +4,7 @@ using System.Text;
 
 namespace Production
 {
-    enum KindFruits
-    {
-        Apple,
-        Apricot,
-        Oranges,
-        Pear,
-        Peach,
-        Tangerines,
-        Mango,
-        Bananas,
-        Lemon,
-        Kiwi
-    }
-
-    enum KindVegetables
-    {
-        Carrots,
-        Beets,
-        Turnips,
-        Cabbage,
-        Tomato,
-        Peppers,
-        Cucumbers,
-        Pumpkin,
-        Squash,
-        Spinach
-    }
-    enum KindBerries
-    {
-        Strawberry,
-        Raspberry,
-        Blackberry,
-        Bluberry,
-        Wildstrawberry,
-        Lingonberries,
-        Grapes,
-        Watermelon,
-        Melon,
-        Barberry
-    }
+  
     public abstract class InfoAboutProduction
     {
         List<InfoAboutProduction> Productins = new List<InfoAboutProduction>();
