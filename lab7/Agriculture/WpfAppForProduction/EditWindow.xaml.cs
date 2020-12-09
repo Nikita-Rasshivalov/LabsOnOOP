067bc2233a47f444597ee990f1d855453971e013
Nikita <nikita_230201@mail.ru>
2020-12-09 23:53:34 +0300

    Third commit

---
+++ lab7/Agriculture/WpfAppForProduction/EditWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using Production;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfAppForProduction
+{
+    /// <summary>
+    /// Логика взаимодействия для EditWindow.xaml
+    /// </summary>
+    public partial class EditWindow : Window
+    {
+        public EditWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+    
+            for (int i = 0; i < MainWindow.Products.Count; i++)
+            {
+                if (ComboBox.SelectedIndex == 0)
+                {
+
+                    MainWindow.Products.Remove(MainWindow.Products[i]);
+
+                }
+
+                if (ComboBox.SelectedIndex == 1)
+                {
+
+                    MainWindow.Products.Remove(MainWindow.Products[i]);
+                }
+
+                if (ComboBox.SelectedIndex == 2)
+                {
+                    MainWindow.Products.Remove(MainWindow.Products[i]);
+
+                }
+            }
+            MessageBox.Show("Product/s was deleated", "Information");
+
+
+
+
+        }
+    }
+}
---
+++ lab7/Agriculture/WpfAppForProduction/Info.cs
@@ -0,0 +1,35 @@
+﻿using Production;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppForProduction
+{
+    public class Info
+    {
+
+
+        public Info(ProdTypes type, string kind, double priceOnKillo, bool technology)
+        {
+            Type = type;
+            Kind = kind;
+            PriceOnKillo = priceOnKillo;
+            if (technology==true)
+            {
+                Technology = "Ecology";
+            }
+            else
+            {
+                Technology = "Not Ecology";
+            }
+        }
+
+        public ProdTypes Type { get; set; }
+        public string Kind { get; set; }
+        public double PriceOnKillo { get; set; }
+
+        public string  Technology { get; set; }
+    }
+}
